4e8150238fdd37fd620ecc3906dd33ad5a6a2ad0
Ke Duong <kelyduong@gmail.com>
2026-02-22 13:49:06 +1100

    Prevent sheet from closing on losing focus

--- src/components/MapDrawer/MapDrawer.tsx
+++ src/components/MapDrawer/MapDrawer.tsx
@@ -23,6 +23,7 @@ export function MapDrawer() {
       <SheetContent
         side="left"
         className={`w-full sm:w-[350px] md:w-[50vw] lg:w-[35vw] z-[800] p-2`}
+        onInteractOutside={(e) => e.preventDefault()}
       >
         <SheetTitle>Route planner</SheetTitle>
         <RoutePlanner />
